c14fce46378962849dd30fc7f9bfbf030e01aa4a
Ivaylo Pashov <ivo.pashoff@gmail.com>
2016-11-08 23:41:21 +0200

    Add the login form
    - it is in a free stanfing module and has some basic validation with error messages

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -1,33 +1,28 @@
 // login.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { UserService } from './user.service';
 
 @Component({
-  selector: 'login',
-  template: `
-  <div class="login jumbotron center-block">
-    <h1>Login</h1>
-    <form role="form" (submit)="login(username.value, password.value)">
-    <div class="form-group">
-      <label for="username">Username</label>
-      <input type="text" #username class="form-control" id="username" placeholder="Username">
-    </div>
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="password" #password class="form-control" id="password" placeholder="Password">
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-  </form>
-  </div>
-  `
+  moduleId: module.id,
+  templateUrl: 'login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router) {}
+  loginForm : FormGroup;
 
-  login(email: string, password: string) {
-    this.router.navigate(['']);
+  constructor(private userService: UserService, private router: Router,private fb: FormBuilder) {
+    let emailRegex = `([a-zA-Z0-9_.]{1}[a-zA-Z0-9_.]*)((@[a-zA-Z]{2}[a-zA-Z]*)[\\\.]([a-zA-Z]{2}|[a-zA-Z]{3}))`;
+    this.loginForm = fb.group({
+      'email' : [null, Validators.compose([Validators.pattern(emailRegex), Validators.required ])],
+      'password' : [null, Validators.compose([Validators.required])],
+    })
+  }
+
+  login(value: any) {
+    console.log(value);
+    // this.router.navigate(['']);
     // this.userService.login(email, password).subscribe((result) => {
     //   if (result) {
     //     this.router.navigate(['']);
--- src/client/app/login/login.module.ts
+++ src/client/app/login/login.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { UserService } from './user.service'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [LoginComponent],
   exports: [LoginComponent],
   providers: [UserService]
