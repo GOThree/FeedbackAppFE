f3d36e7b555941045ebf9cd37f6d3c9eb4eacd29
Vladimir Orelov <vladimirorelov@gmail.com>
2016-11-08 00:20:18 +0000

    initial items-review component

---
+++ src/client/app/review/items-review/items-review.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'items-review',
+  templateUrl: 'items-review.component.html'
+})
+export class ItemsReviewComponent{}
