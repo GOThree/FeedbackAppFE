532da4510c2037fa4713857d57dca62143dd2348
Vladimir Orelov <vladimirorelov@gmail.com>
2016-11-09 00:54:43 +0000

    simple visualization of the mocks

--- src/client/app/shared/product-list/product-list.service.ts
+++ src/client/app/shared/product-list/product-list.service.ts
@@ -1,10 +1,9 @@
 import {Injectable} from '@angular/core';
-import {MENU} from "./index";
+import {ProductListMock} from "./index";
 
 @Injectable()
-export class MenuService{
+export class ProductListService{
   getProductList(id: number): Promise<any> {
-    MENU.push(id);
-    return Promise.resolve(MENU);
+    return Promise.resolve(ProductListMock);
   }
 }
