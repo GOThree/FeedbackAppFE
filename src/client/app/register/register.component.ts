4a5e4b6de8563610fb3b5774e9a32f1ee223d371
Ivaylo Pashov <ivo.pashoff@gmail.com>
2016-11-10 09:31:34 +0200

    Made the password - confirm password validation on the register screen

--- src/client/app/register/register.component.ts
+++ src/client/app/register/register.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { UserService } from './../shared/auth/user.service';
-// import {Control, ControlGroup} from '@angular/common';
 
 @Component({
   moduleId: module.id,
@@ -16,24 +15,31 @@ export class RegisterComponent {
     let emailRegex = `([a-zA-Z0-9_.]{1}[a-zA-Z0-9_.]*)((@[a-zA-Z]{2}[a-zA-Z]*)[\\\.]([a-zA-Z]{2}|[a-zA-Z]{3}))`;
     this.registerForm = fb.group({
      'email' : [null, Validators.compose([Validators.pattern(emailRegex), Validators.required ])],
-     'password' : [null, Validators.compose([Validators.required])],
      'firstName' : [null, Validators.compose([Validators.required])],
      'lastName' : [null, Validators.compose([Validators.required])],
-      // passwords: fb.group({
-      //   'password': [null, Validators.required],
-      //   'confirmPassword': [null, Validators.required]
-      // }, {validator: this.areEqual})
+      'passwords': fb.group({
+        'password': [null, Validators.compose([Validators.required])],
+        'confirmPassword': [null, Validators.compose([Validators.required])]
+      }, {validator: this.areEqual})
     })
   }
 
-  // areEqual(group: ControlGroup) {
-  //   return true;
-  // }
+  areEqual(group: any) {
+    console.log('in are equal')
+    if (group.controls.password.value && (group.controls.password.value == group.controls.confirmPassword.value)) {
+        return null;
+    }
+    else {
+      return {
+          areEqual: false
+        };
+    }
+  }
 
   register(value: any) {
     console.log(this.registerForm)
     console.log(value);
-    // this.userService.register(email, password).subscribe((result) => {
+    // this.userService.register(value.firstName, value.lastName, value.email, value.passwords.password, value.passwords.confirmPassword).subscribe((result) => {
     //   if (result) {
     //     this.router.navigate(['']);
     //   }
