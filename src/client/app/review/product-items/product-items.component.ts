740ca7d3d0574a48bc452fe6fb84e4c706e080d5
Vladimir Orelov <vladimirorelov@gmail.com>
2016-11-13 10:28:38 +0000

    implement product list items filtering

--- src/client/app/review/product-items/product-items.component.ts
+++ src/client/app/review/product-items/product-items.component.ts
@@ -1,17 +1,36 @@
-import {Component, Input, OnChanges, SimpleChange } from '@angular/core'
-import {ProductList} from "../../shared/models/product-list";
+import {Component, Input, OnChanges, SimpleChange} from '@angular/core'
+import {ProductList, ProductListItem} from "../../shared/models/index";
+import {FormControl} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'product-items',
   templateUrl: 'product-items.component.html'
 })
-export class ProductItemsComponent implements OnChanges{
+export class ProductItemsComponent implements OnChanges, OnInit {
   @Input() productList:ProductList;
 
-  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
-    for(let propName in changes) {
-      console.log(propName)
+  term = new FormControl();
+  private _items:ProductListItem[];
+
+  ngOnInit() {
+    this.term.valueChanges
+      .subscribe(value => {
+        // cache into _items since the
+        // productList.items gonna be filtered
+        if (!this._items) {
+          this._items = this.productList.items.slice();
+        }
+
+        // filter items that the title contains the value.
+        this.productList.items = this._items.filter(function (obj) {
+          return obj.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+        });
+      });
+  }
+
+  ngOnChanges(changes:{[propKey:string]:SimpleChange}) {
+    for (let propName in changes) {
       this[propName] = changes[propName].currentValue;
     }
   }
