5e2ecf244ed62ee32ac99c44823cba54a8ea308b
Ivaylo Pashov <ivo.pashoff@gmail.com>
2016-11-21 23:10:18 +0200

    Clear out the navbar component
    - missed that with my last PR that cleared unnecessary
    stuff from the app

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { NavbarComponent } from './navbar/index';
 import {AutoCompleteComponent} from "./auto-complete/auto-complete.component";
 import {EmailComponent} from "./email/email.component";
 import {PhoneComponent} from "./phone/phone.component";
@@ -11,9 +10,8 @@ import {CommentComponent} from "./comment/comment.component";
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [NavbarComponent, AutoCompleteComponent, EmailComponent, PhoneComponent, CommentComponent],
-  exports: [NavbarComponent,
-    CommonModule, FormsModule, RouterModule, AutoCompleteComponent, EmailComponent, PhoneComponent, CommentComponent]
+  declarations: [AutoCompleteComponent, EmailComponent, PhoneComponent, CommentComponent],
+  exports: [CommonModule, FormsModule, RouterModule, AutoCompleteComponent, EmailComponent, PhoneComponent, CommentComponent]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
