e82acb9dafaf231d4ca8b30f95cd34af6b1e6a61
Vladimir Orelov <vladimirorelov@gmail.com>
2016-12-02 23:08:48 +0000

    clean home component

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { GoogleService } from '../shared/globals/google';
 
 @Component({
   moduleId: module.id,
   selector: 'sd-home',
   templateUrl: 'home.component.html',
-  providers: [GoogleService]
 })
 
 export class HomeComponent {
-
-  constructor(private googleService: GoogleService) {
-    console.log(googleService)
-  }
 }
 
 
