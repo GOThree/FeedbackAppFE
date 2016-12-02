8afa0d486419affba6fdaab7fe50e4883264b22f
Vladimir Orelov <vladimirorelov@gmail.com>
2016-12-02 23:11:45 +0000

    update ac-places directive to work with ngControl elements

--- src/client/app/shared/ac-places/ac-places.directive.ts
+++ src/client/app/shared/ac-places/ac-places.directive.ts
@@ -1,13 +1,31 @@
-import {Directive, ElementRef, Input, Renderer} from '@angular/core';
+import {Directive, Output, EventEmitter, ElementRef, Input, Renderer, HostListener, OnInit} from '@angular/core';
 import {GoogleService} from "../globals/index";
+import { NgControl } from "@angular/forms";
 
 @Directive({
   selector: '[ac-places]',
   providers: [GoogleService]
 })
-export class PlacesDirective {
-  constructor(el: ElementRef, renderer: Renderer, googleService: GoogleService) {
-    let element = el.nativeElement;
-    let autocomplete = new googleService.placesService.Autocomplete(element);
+export class PlacesDirective implements OnInit {
+  @Output() place: EventEmitter<any> = new EventEmitter<any>();
+  private autocomplete: any;
+
+  constructor(private el: ElementRef,
+              private  googleService: GoogleService,
+              private control: NgControl) {
+  }
+
+  ngOnInit() {
+    let element = this.el.nativeElement;
+    this.autocomplete = new this.googleService.placesService.Autocomplete(element);
+    this.autocomplete.addListener('place_changed', () => {
+      var place = this.autocomplete.getPlace();
+
+      this.control.control.setValue(place, {emitModelToViewChange: false});
+      console.log("Control: ", this.control);
+
+      this.place.emit(place);
+    });
   }
 }
+
