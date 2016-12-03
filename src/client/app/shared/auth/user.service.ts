2dda98e55f275161b9b5e80f2a4bc237869595ca
Ivaylo Pashov <ivo.pashoff@gmail.com>
2016-12-04 00:29:26 +0200

    Modify the implementation of how UserService knows if you are logged in

--- src/client/app/shared/auth/user.service.ts
+++ src/client/app/shared/auth/user.service.ts
@@ -5,11 +5,8 @@ import { ENDPOINTS } from '../../global.vars'
 
 @Injectable()
 export class UserService {
-  private loggedIn = false;
 
-  constructor(private http: HttpClient) {
-    this.loggedIn = !!localStorage.getItem('auth_token');
-  }
+  constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
     let headers = new Headers();
@@ -32,7 +29,6 @@ export class UserService {
         let body_as_json = res.json()
         if (res.ok) {
           localStorage.setItem('access_token', body_as_json.access_token);
-          this.loggedIn = true;
         }
         return body_as_json
       });
@@ -74,13 +70,20 @@ export class UserService {
         }
       })
   }
+
+  changePassword(oldPassword: string, newPassword: string) {
+    return this.http
+      .post(
+        ENDPOINTS.changePassword, 
+        JSON.stringify({ oldPassword, newPassword }), 
+      )
+  }
   
   logout() {
-    localStorage.removeItem('auth_token');
-    this.loggedIn = false;
+    localStorage.removeItem('access_token');
   }
 
   isLoggedIn() {
-    return this.loggedIn;
+    return !!localStorage.getItem('access_token');
   }
 }
