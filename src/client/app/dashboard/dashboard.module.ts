c9cd18a55e1357ab9bc5be1b5996384b4a806696
Ivaylo Pashov <ivo.pashoff@gmail.com>
2016-12-12 23:32:05 +0200

    Remove theme module leftovers

--- src/client/app/dashboard/dashboard.module.ts
+++ src/client/app/dashboard/dashboard.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
-import { NgaModule } from '../theme/index';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, NgaModule],
+  imports: [CommonModule, SharedModule],
   declarations: [DashboardComponent],
 })
 export class DashboardModule { }
